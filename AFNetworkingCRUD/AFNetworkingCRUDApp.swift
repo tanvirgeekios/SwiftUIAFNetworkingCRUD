8d5229163ee55075f1b23fc8daddfa0bc7bfba1e
tanvirgeekios <tanvir.alam@iosventure.com>
2020-12-21 12:24:02 +0600

    Initial Commit

---
+++ AFNetworkingCRUD/AFNetworkingCRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFNetworkingCRUDApp.swift
+//  AFNetworkingCRUD
+//
+//  Created by MD Tanvir Alam on 21/12/20.
+//
+
+import SwiftUI
+
+@main
+struct AFNetworkingCRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
